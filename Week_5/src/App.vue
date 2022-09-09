1f24b900c1fd540bd52d20ceca5406096ad193ae
JohnJackson685 <109631279+JohnJackson685@users.noreply.github.com>
2022-09-09 19:34:20 +1000

    Add files via upload

---
+++ Week_5/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="HelloWorld">
+    <HelloWorld></HelloWorld>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    
+    HelloWorld
+  }
+}
+</script>
+
---
+++ Week_5/src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<template>
+  
+  <h1>HelloWorld</h1>
+
+  <button @click="button">Click this button</button>
+  <p>Click counter: {{clicks}}</p>
+  
+  <input v-model="email">
+
+  <p> Copy: {{email}}</p>
+
+</template>
+
+<script>
+
+export default {
+  
+  data() {
+    return {
+      clicks: 0,
+      email: ''
+    }
+  },
+
+  methods: {
+    button() {
+      this.clicks++;
+    }
+  }
+}
+</script>
+
+
+
+
